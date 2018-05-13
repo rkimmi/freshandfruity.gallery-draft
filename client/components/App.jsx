6df16f71a8929d339cd6065df1e314c0ffbcc02d
rkimmi <kimmirindel97@gmail.com>
2018-05-13 16:36:37 +1200

    removed redux

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const App = () => (
   <div className='app-container'>
-    <p>Hiya, good luck!</p>
-    <iframe src="https://giphy.com/embed/yoJC2AkZpy7PffDToY" width="480" height="359" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+    <h1>hi!</h1>
   </div>
 )
 
